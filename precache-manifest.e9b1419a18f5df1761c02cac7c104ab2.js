600804beea7d340cc7f01cabbbf5728f3f616d72
MikaCinc <mihajlo.ls00@outlook.com>
2019-12-29 03:17:17 +0100

    Updates

---
+++ precache-manifest.e9b1419a18f5df1761c02cac7c104ab2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c01b0ae27b1a73d017dea0e0b0f867",
+    "url": "/durango/index.html"
+  },
+  {
+    "revision": "e24d0d74a4e838ffc9b0",
+    "url": "/durango/static/css/main.90c079c6.chunk.css"
+  },
+  {
+    "revision": "76814a8068e2a55e684e",
+    "url": "/durango/static/js/2.e4081139.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/durango/static/js/2.e4081139.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e24d0d74a4e838ffc9b0",
+    "url": "/durango/static/js/main.7ab8063a.chunk.js"
+  },
+  {
+    "revision": "c854f09ffe5643a05685",
+    "url": "/durango/static/js/runtime-main.faf610aa.js"
+  }
+]);
