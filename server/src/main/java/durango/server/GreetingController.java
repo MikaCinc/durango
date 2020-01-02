5c07257a3e2e6227c75c50952ad812ae58f19b96
Luka <lukakocic@live.com>
2020-01-02 22:02:59 +0100

    hotfix

--- server/src/main/java/durango/server/GreetingController.java
+++ server/src/main/java/durango/server/GreetingController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
+@CrossOrigin(origins = "http://localhost")
 @RestController
 public class GreetingController {
     @Autowired
@@ -14,45 +15,38 @@ public class GreetingController {
     private UserService userService;
 
     //Cafe connection handler
-    @CrossOrigin(origins = "http://localhost:7000")
     @GetMapping("/lista-kafica")
     public ArrayList<Kafic> listaKafica(){
         return kaficService.getKafici();
     }
 
-    @CrossOrigin(origins = "http://localhost:7000")
     @GetMapping("/lista-detalja")
     public ArrayList<KaficDetalji> listaDetalja(){
         return kaficService.getKaficiDetaljiList();
     }
 
-    @CrossOrigin(origins = "http://localhost:7000")
     @GetMapping("/kafic/{id}")
     public Kafic getKafic(@PathVariable int id){
         return kaficService.getKafic(id);
     }
 
-    @CrossOrigin(origins = "http://localhost:7000")
     @GetMapping("/kafic-detalji/{id}")
     public KaficDetalji getDetalje(@PathVariable int id){
         return kaficService.getKaficDetalji(id);
     }
 
-    @CrossOrigin(origins = "http://localhost:7000")
     @PostMapping(value = "/kafic")
     public void kreirajKafic(@RequestBody Kafic kafic, KaficDetalji kaficDetalji){
         kaficService.kreirajKafic(kafic, kaficDetalji);
     }
 
     //User connection handler
-    @CrossOrigin(origins = "http://localhost:7000")
     @GetMapping("/korisnik/{id}")
     public User getUserInfo(@PathVariable int id){
         return userService.getUser(id);
     }
 
     @PostMapping("/kreiraj-korisnika")
-    @CrossOrigin(origins = "http://localhost:7000")
     public int kreirajKorisnika(@RequestBody User user){
         return userService.createUser(user);
     }
