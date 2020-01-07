7f5db909da9fd4a8bc1fe71df84a866d4b925395
MikaCinc <mihajlo.ls00@outlook.com>
2020-01-07 18:34:35 +0100

    added localStorage for User

--- src/LoginScreen/index.js
+++ src/LoginScreen/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import GoogleLogin from 'react-google-login';
 import FacebookLogin from 'react-facebook-login';
@@ -12,19 +12,44 @@ const LoginScreen = props => {
     const [profileImage, setProfileImage] = useState('');
     const [profileName, setProfileName] = useState('');
 
+    useEffect(() => {
+        // localStorage.removeItem('User')
+        let User = JSON.parse(localStorage.getItem('User'));
+
+        if (User && User.id) {
+            setIsSuccess(true);
+            setProfileImage(User.imageUrl)
+            setProfileName(User.name)
+            setTimeout(() => {
+                props.setAuthorized(true);
+            }, 2000)
+        }
+
+    }, []);
+
     const responseGoogleSuccess = (response) => {
         // console.log(response)
         setIsSuccess(true);
         setProfileImage(response.profileObj.imageUrl)
         setProfileName(response.profileObj.name)
+
+        let obj = {
+            id: response.profileObj.googleId,
+            email: response.profileObj.email,
+            imageUrl: response.profileObj.imageUrl,
+            name: response.profileObj.name
+        };
+
+        localStorage.setItem('User', JSON.stringify(obj));
+
         setTimeout(() => {
             props.setAuthorized(true);
-        }, 2000)
+        }, 2000);
     }
 
     const responseGoogleFailure = (response) => {
         // console.log(response)
-        alert('Something is wrong with Google login')
+        alert('Something is wrong with Google login');
     }
 
     const responseFacebook = (response) => {
@@ -53,7 +78,7 @@ const LoginScreen = props => {
             bottom
             duration={1000}
         >
-            <div>
+            <div className="loginDetailsDiv">
                 <img
                     className="loginProfileImageLoaded"
                     src={profileImage}
@@ -61,53 +86,57 @@ const LoginScreen = props => {
                 <p className="loginParagraph">{profileName}</p>
             </div>
         </Zoom>
-        <GoogleLogin
-            clientId="873302302315-mv8lh0772kbe55nvh687qpv8ddvlv9t0.apps.googleusercontent.com"
-            buttonText="Login"
-            onSuccess={responseGoogleSuccess}
-            onFailure={responseGoogleFailure}
-            cookiePolicy={'single_host_origin'}
-            render={renderProps => (
-                <div
-                    className="google-btn"
-                    onClick={renderProps.onClick}
-                    disabled={renderProps.disabled}
-                >
-                    <img
-                        className="google-icon"
-                        src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-                    />
-                    <p className="google-text"><b>Sign in with Google</b></p>
-                </div>
-            )}
-        />
-        <FacebookLogin
-            appId="503529696932140"
-            autoLoad={false}
-            fields="name,email,picture"
-            authType="reauthenticate"
-            tag={
-                renderProps => (
-                    <div
-                        className="facebook-btn"
-                        onClick={renderProps.onClick}
-                    >
-                        <img
-                            className="facebook-icon"
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/F_icon.svg/267px-F_icon.svg.png"
-                        />
-                        <p className="facebook-text"><b>Sign in with Facebook</b></p>
-                    </div>
-                )
-            }
-            /* onClick={(nesto) => {
-                console.log('nesto', nesto)
-            }} */
-            callback={responseFacebook}
-            onFailure={() => {
-                console.log('Something is wrong with Facebook sign in')
-            }}
-        />
+        {
+            !isSuccess && <Fragment>
+                <GoogleLogin
+                    clientId="873302302315-mv8lh0772kbe55nvh687qpv8ddvlv9t0.apps.googleusercontent.com"
+                    buttonText="Login"
+                    onSuccess={responseGoogleSuccess}
+                    onFailure={responseGoogleFailure}
+                    cookiePolicy={'single_host_origin'}
+                    render={renderProps => (
+                        <div
+                            className="google-btn"
+                            onClick={renderProps.onClick}
+                            disabled={renderProps.disabled}
+                        >
+                            <img
+                                className="google-icon"
+                                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                            />
+                            <p className="google-text"><b>Sign in with Google</b></p>
+                        </div>
+                    )}
+                />
+                <FacebookLogin
+                    appId="503529696932140"
+                    autoLoad={false}
+                    fields="name,email,picture"
+                    authType="reauthenticate"
+                    tag={
+                        renderProps => (
+                            <div
+                                className="facebook-btn"
+                                onClick={renderProps.onClick}
+                            >
+                                <img
+                                    className="facebook-icon"
+                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/F_icon.svg/267px-F_icon.svg.png"
+                                />
+                                <p className="facebook-text"><b>Sign in with Facebook</b></p>
+                            </div>
+                        )
+                    }
+                    /* onClick={(nesto) => {
+                        console.log('nesto', nesto)
+                    }} */
+                    callback={responseFacebook}
+                    onFailure={() => {
+                        console.log('Something is wrong with Facebook sign in')
+                    }}
+                />
+            </Fragment>
+        }
     </div>;
 };
 
