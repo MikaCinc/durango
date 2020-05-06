afa7e71e1366ed1861f9e72d52ca625785c8a83e
MikaCinc <mihajlo.ls00@outlook.com>
2020-05-06 19:48:00 +0200

    old server folder deleted

--- server/src/main/java/durango/server/KaficRepository.java
+++
@@ -1,7 +0,0 @@
-package durango.server;
-
-import durango.server.data.Kafic;
-import org.springframework.data.repository.CrudRepository;
-
-public interface KaficRepository extends CrudRepository<Kafic, Integer> {
-}
--- server/src/main/java/durango/server/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package durango.server;
-
-import durango.server.data.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer>{
-
-
-}
